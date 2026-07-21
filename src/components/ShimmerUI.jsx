9dfdfec61e0b65f5765f3cecaebb9ebf0d1c21f5
Yashwanth S <216149987+yashwanths1101@users.noreply.github.com>
2026-07-21 20:59:42 +0530

    feat: add shimmer UI

---
+++ src/components/ShimmerUI.jsx
@@ -0,0 +1,17 @@
+const ShimmerUI = () => {
+  return (
+    <div className='shimmer'>
+      <div className='shimmerHeading-container'>
+        <div className='shimmerHeading1'></div>
+        <div className='shimmerHeading2'></div>
+      </div>
+      <div className='shimmerBody-container'>
+        {Array.from({ length: 10 }).map((_, i) => (
+          <div className='shimmer-card' key={i}></div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default ShimmerUI
