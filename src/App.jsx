ffff20e12df332b513285f237f51b2fa7ad58937
Yashwanth S <216149987+yashwanths1101@users.noreply.github.com>
2026-07-28 20:26:45 +0530

    refactor: change structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import HeaderComponent from './components/Header/Header'
+import HeaderComponent from './components/Header'
 import BodyComponent from './components/Body'
 import { useState } from 'react'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Error from './components/Error'
+
 const AppLayout = () => {
   return (
     <div className='app'>
       <HeaderComponent />
-      <BodyComponent />
+      <Outlet />
     </div>
   )
 }
 
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/',
+        element: <BodyComponent />
+      },
+      {
+        path: '/about',
+        element: <About />
+      },
+      {
+        path: '/contact',
+        element: <Contact />
+      }
+    ]
+  }
+])
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<AppLayout />)
+root.render(<RouterProvider router={appRouter} />)
